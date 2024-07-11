ea81cb597d11724a8b7412ef6e45bd7344be0f7b
Devil980 <timilsina.anish26@gmail.com>
2024-07-11 17:20:14 +0545

    Update news.py

--- news.py
+++ news.py
@@ -2,7 +2,7 @@ import requests
 import json
 query = input("Which news do you wanna know about? ")
 # source = input("Which source you wanna know from? ")
-api_key = input("Enter Your API key: ")
+api_key = input("Enter Your API key: ") #You can get your own personal api in newsapi.org
 url = (f"https://newsapi.org/v2/everything?q={query}&from=2024-02-29&sortBy=publishedAt&apiKey=bd9dae65e60f4a25b8f9a231b2d88e93")
 r = requests.get(url)
 news = json.loads(r.text)
